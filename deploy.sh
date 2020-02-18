a1a7a716fa8e488fa066a3d08ac565039673cb9f
pamelamartello <tlacuachemojao@gmail.com>
2020-02-18 14:36:22 +0800

    rebuilding site Tue, 18 Feb, 2020 14:36:21

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin develop
+git subtree split -P public -b published
+git push origin published:master --force
+git branch -D published
